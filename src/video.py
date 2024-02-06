88dae87d4acd4f02cc9e848ef8e984170c0b3e72
Egor Arhipov <111359850+Maurdihen@users.noreply.github.com>
2024-02-06 15:57:52 +0300

    homework_16.1

--- src/video.py
+++ src/video.py
@@ -10,12 +10,18 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=API_KEY)
 
     def __init__(self, id_video):
-        self.id_video = id_video
-        self._video = self.youtube.videos().list(part='snippet,statistics', id=self.id_video).execute()
-        self.title = self._video['items'][0]['snippet']['title']
-        self.url = self._video['items'][0]['snippet']['thumbnails']['default']['url']
-        self.view_count = self._video['items'][0]['statistics']['viewCount']
-        self.likes_count = self._video['items'][0]['statistics']['likeCount']
+        try:
+            self.id_video = id_video
+            self._video = self.youtube.videos().list(part='snippet,statistics', id=self.id_video).execute()
+            self.title = self._video['items'][0]['snippet']['title']
+            self.url = self._video['items'][0]['snippet']['thumbnails']['default']['url']
+            self.view_count = self._video['items'][0]['statistics']['viewCount']
+            self.likes_count = self._video['items'][0]['statistics']['likeCount']
+        except Exception as ex:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.likes_count = None
 
     def __str__(self):
         return self.title
